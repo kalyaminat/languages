d96747f14b09fe1dd0914c5fc2a79dbe577bcc49
kalyaminat <kalyaminat@yandex.ru>
2025-05-20 20:36:40 +0300

    Choose language

---
+++ test_items.py
@@ -0,0 +1,17 @@
+import pytest
+from selenium.webdriver.common.by import By
+import time
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.chrome.options import Options
+link = ' http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
+
+
+def test_is_button_visible(browser):
+    time.sleep(30)
+    browser.get(link)
+    element = WebDriverWait(browser, 15).until(
+        EC.visibility_of_all_elements_located((By.CSS_SELECTOR, '[type="submit"]')))
+
+
+    assert len(element) > 0
